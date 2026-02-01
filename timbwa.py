5dc669fdfbd21df72525cba4718a3271ea9eb96f
timholchris-ai <timholchris@gmail.com>
2026-02-01 11:11:28 +0300

    Update timbwa.py

--- timbwa.py
+++ timbwa.py
@@ -1,29 +1,37 @@
-# Parent class
+# Base class: Square
 class Square:
     def __init__(self, side):
         self.side = side
+        print("Square object created")
 
+    # Method to calculate perimeter
     def getPeri(self):
         return 4 * self.side
 
+    # Method to calculate area
     def getArea(self):
-        return self.side * self.side
+        return self.side ** 2
+
+    # Destructor
+    def __del__(self):
+        print("Square object destroyed")
 
 
-# Child class
+# Derived class: Cube inherits from Square
 class Cube(Square):
     def __init__(self, side):
-        super().__init__(side)
+        super().__init__(side)  # Call Square constructor
+        print("Cube object created")
 
+    # Method to calculate surface area of cube
     def getArea(self):
-        return 6 * self.side * self.side
+        return 6 * super().getArea()  # 6 * area of one face
 
+    # Method to calculate volume
     def getVolume(self):
-        return self.side * self.side * self.side
+        return self.side ** 3
 
-
-c = Cube(5)
-
-print("Perimeter of base:", c.getPeri())
-print("Surface area:", c.getArea())
-print("Volume:", c.getVolume())
+    # Destructor
+    def __del__(self):
+        print("Cube object destroyed")
+        super().__del__()  # Call base class destructor
